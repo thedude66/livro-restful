cad9246d0759e9f0a7cc58afb50b0cd275400457
danielps <danieljfa@gmail.com>
2013-01-30 18:47:56 -0200

    rest-base

---
+++ rest-base/classes/Teste.php
@@ -0,0 +1,8 @@
+<?php
+class Teste
+{
+	function helloWorld($nome)
+	{
+		return "Hello World, $nome";
+	}
+}
---
+++ rest-base/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim(array(
+    'debug' => false
+));
+
+
+
+$app->error(function ( Exception $e ) use ($app) {
+
+    $erroObj = new stdClass();
+    $erroObj->message = $e->getMessage();
+    //$erroObj->trace = $e->getTraceAsString();
+    $erroObj->file = $e->getFile();
+    $erroObj->line = $e->getLine();
+
+    echo "{'error':".json_encode($erroObj)."}";    
+});
+
+
+$app->get('/:controller/:action(/:parameter)', function ($controller, $action, $parameter=null) {
+
+	include_once "classes/{$controller}.php";
+	$classe = new $controller();
+	$retorno = call_user_func_array(array($classe,$action),array($parameter));
+	echo "{'result':".json_encode($retorno)."}";
+
+});
+
+$app->run();
