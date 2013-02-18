b4339058317f9d055bd4fe7f6f41d1068dad9ecd
danielps <danieljfa@gmail.com>
2013-02-18 17:16:04 -0300

    Detalhes da venda

--- sistema-vendas-jquery/js/vendasAdmin.js
+++ sistema-vendas-jquery/js/vendasAdmin.js
@@ -17,7 +17,7 @@ function atualizaGrid() {
         success: function(data) {
             $("#tableVendas").find("tbody tr").remove();
             data.result.forEach(function(venda) {
-                
+
                 row = "<tr>"
                         + "<td><a id='edit' data-id='" + venda.id + "' href='#'><i class='icon-info-sign'></i></a></td>"
                         + "</td><td>" + venda.dataVenda + "</td>"
@@ -26,7 +26,7 @@ function atualizaGrid() {
                         + "</tr>";
                 $("#tableVendas > tbody:last").append(row);
             });
-            
+
         },
         error: function(result) {
             $("#tableVendas").find("tbody tr").remove();
@@ -37,5 +37,54 @@ function atualizaGrid() {
 
 
 }
+
+$("#edit").live("click", function() {
+
+    $("#errorVenda").hide();
+
+    row = $(this);
+    idVenda = row.attr("data-id");
+    row.html('<img src="img/ajax-loader.gif">');
+    
+     $("#detalhes").hide();
+    $("#detalhes_venda").html("");
+    $("#itens").html("");
+    
+
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        url: rootUrl + "venda/list/" + idVenda,
+        success: function(data) {
+            venda = data.result;
+            $("#detalhes").show();
+            
+           $("#detalhes_venda").append("<dt>Id da venda:</dt><dd>" + venda.id + "</dd>");
+           $("#detalhes_venda").append("<dt>Data:</dt><dd>" + venda.dataVenda + "</dd>");
+           $("#detalhes_venda").append("<dt>Vendedor:</dt><dd>" + venda.nomeVendedor + "</dd>");
+           $("#detalhes_venda").append("<dt>Cliente:</dt><dd>" + venda.nomeCliente + "</dd>");
+           
+           total = 0;
+           venda.itens.forEach(function(item){
+               total += item.quantidade*item.precoUnitario;
+            $("#itens").append('<div class="alert alert-gray">' + item.nome + '</strong><span class="pull-right">' + item.quantidade + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;R$ ' + item.precoUnitario + '</span>');
+           });
+           
+           $("#itens").append('<span class="alert alert-gray pull-right">Total: R$ '+total+'</span>');
+           
+           
+           
+            
+        },
+        error: function(result) {
+            $("#errorVenda").html(getErrorMessage(result.responseText));
+            $("#errorVenda").show();
+        },
+        complete: function() {
+            row.html("<i class='icon-info-sign'>");
+        }
+    });
+
+});
 
 
--- sistema-vendas-server/classes/Venda.php
+++ sistema-vendas-server/classes/Venda.php
@@ -6,7 +6,7 @@ class Venda {
 
         $sqlVenda = "INSERT INTO vendas (idCliente,idVendedor,dataVenda) VALUES (:idCliente,:idVendedor,:dataVenda)";
         $sqlItemVenda = "INSERT INTO itensVenda (idVenda,idProduto,quantidade,precoUnitario) VALUES (:idVenda,:idProduto,:quantidade,:precoUnitario)";
-        
+
         try {
 
             DB::beginTransaction();
@@ -16,9 +16,9 @@ class Venda {
             $stmtVendas->bindParam("idVendedor", $venda->idVendedor);
             $stmtVendas->bindParam("dataVenda", DB::dateToMySql($venda->data));
             $stmtVendas->execute();
-            
+
             $venda->idVenda = DB::lastInsertId();
-            
+
             foreach ($venda->itens as $item) {
                 $stmtItem = DB::prepare($sqlItemVenda);
                 $stmtItem->bindParam("idVenda", $venda->idVenda);
@@ -29,25 +29,43 @@ class Venda {
             }
 
             DB::commit();
-            
         } catch (Exception $exc) {
             DB::rollBack();
             throw new Exception($exc->getMessage());
         }
-        
+
         return $venda;
     }
-    
-    public function get_listAll(){
-        
+
+    public function get_listAll() {
+
         $sql = "SELECT v.id,DATE_FORMAT(v.dataVenda, '%d/%m/%Y') as dataVenda,u.nome as nomeCliente,u2.nome as nomeVendedor FROM vendas v,clientes c,vendedores e, usuarios u, usuarios u2 
             WHERE u.id=c.idUsuario and c.id=v.idCliente 
             and u2.id=e.idUsuario and e.id=v.idVendedor";
-        
+
         $stmt = DB::prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
-            
+
+    public function get_list($id) {
+
+        $sql = "SELECT v.id,DATE_FORMAT(v.dataVenda, '%d/%m/%Y') as dataVenda,u.nome as nomeCliente,u2.nome as nomeVendedor FROM vendas v,clientes c,vendedores e, usuarios u, usuarios u2 
+            WHERE u.id=c.idUsuario and c.id=v.idCliente 
+            and u2.id=e.idUsuario and e.id=v.idVendedor and v.id=:id";
+
+        $stmt = DB::prepare($sql);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $venda = $stmt->fetch();
+
+        $sqlItens = "SELECT i.*,p.nome FROM itensVenda i,produtos p WHERE idVenda=:id and i.idProduto=p.id";
+        $stmtItens = DB::prepare($sqlItens);
+        $stmtItens->bindParam("id", $id);
+        $stmtItens->execute();
+        $venda->itens = $stmtItens->fetchAll();
+
+        return $venda;
+    }
 
 }
