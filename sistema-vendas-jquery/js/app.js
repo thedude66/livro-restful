37593c32d7b3e3ba75cc09bad002d410be1b524d
danielps <danieljfa@gmail.com>
2013-02-22 10:46:39 -0300

    formatacao

--- sistema-vendas-jquery/js/app.js
+++ sistema-vendas-jquery/js/app.js
@@ -52,6 +52,48 @@ function getDescTipo($tipo)
 
 }
 
+
+
+//retirado de: http://codigosprontos.blogspot.com.br/2010/06/o-codigo-function-moedavalor-casas.html
+function moeda(valor) {
+    casas = 2;
+    separdor_decimal = ",";
+    separador_milhar = ".";
+
+    var valor_total = parseInt(valor * (Math.pow(10, casas)));
+    var inteiros = parseInt(parseInt(valor * (Math.pow(10, casas))) / parseFloat(Math.pow(10, casas)));
+    var centavos = parseInt(parseInt(valor * (Math.pow(10, casas))) % parseFloat(Math.pow(10, casas)));
+
+
+    if (centavos % 10 == 0 && centavos + "".length < 2) {
+        centavos = centavos + "0";
+    } else if (centavos < 10) {
+        centavos = "0" + centavos;
+    }
+
+    var milhares = parseInt(inteiros / 1000);
+    inteiros = inteiros % 1000;
+
+    var retorno = "";
+
+    if (milhares > 0) {
+        retorno = milhares + "" + separador_milhar + "" + retorno
+        if (inteiros == 0) {
+            inteiros = "000";
+        } else if (inteiros < 10) {
+            inteiros = "00" + inteiros;
+        } else if (inteiros < 100) {
+            inteiros = "0" + inteiros;
+        }
+    }
+    retorno += inteiros + "" + separdor_decimal + "" + centavos;
+
+
+    return retorno;
+
+}
+
+
 $(document).ready(function() {
 
     $("#linkSair").click(function() {
@@ -72,7 +114,7 @@ $(document).ready(function() {
     /*
      Previne que um enter poste o formulário
      Em ambientes jquery e ajax, enter 
-     submetendo formulários não são bem vndos
+     submetendo formulários não são bem vindos
      */
     $(window).keydown(function(event) {
         if (event.keyCode == 13) {
@@ -80,50 +122,7 @@ $(document).ready(function() {
             return false;
         }
     });
-
-
 });
-
-//retirado de: http://codigosprontos.blogspot.com.br/2010/06/o-codigo-function-moedavalor-casas.html
-function moeda(valor){ 
-    casas=2;
-    separdor_decimal=",";
-    separador_milhar=".";
- 
- var valor_total = parseInt(valor * (Math.pow(10,casas)));
- var inteiros =  parseInt(parseInt(valor * (Math.pow(10,casas))) / parseFloat(Math.pow(10,casas)));
- var centavos = parseInt(parseInt(valor * (Math.pow(10,casas))) % parseFloat(Math.pow(10,casas)));
- 
-  
- if(centavos%10 == 0 && centavos+"".length<2 ){
-  centavos = centavos+"0";
- }else if(centavos<10){
-  centavos = "0"+centavos;
- }
-  
- var milhares = parseInt(inteiros/1000);
- inteiros = inteiros % 1000; 
- 
- var retorno = "";
- 
- if(milhares>0){
-  retorno = milhares+""+separador_milhar+""+retorno
-  if(inteiros == 0){
-   inteiros = "000";
-  } else if(inteiros < 10){
-   inteiros = "00"+inteiros; 
-  } else if(inteiros < 100){
-   inteiros = "0"+inteiros; 
-  }
- }
-  retorno += inteiros+""+separdor_decimal+""+centavos;
- 
- 
- return retorno;
- 
-}
-
-
 
 
 
